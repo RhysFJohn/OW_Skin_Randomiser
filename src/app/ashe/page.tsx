c42730565be0fc5d5ad4df4853a5234c8674ee81
RhysFJohn <r.freeman-john@outlook.com>
2024-02-03 20:55:59 +0000

    Image correction for responsive sizings

--- src/app/ashe/page.tsx
+++ src/app/ashe/page.tsx
@@ -9,12 +9,19 @@ const Ashe = () => {
   
   return (
     <div className='flex min-h-screen flex-col items-center bg-ashe bg-cover'>
-      <h1 className='text-xl bold p-4'>Ashe</h1>
-      <div className="grid grid-cols-2 p-4">
-        <div className="flex flex-col">
-          <Image src={randomAsheSkin.image} alt={randomAsheSkin.name} width={750} height={750} />
+      <h1 className='text-3xl bold p-4'>Ashe</h1>
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 p-4">
+        <div className="flex flex-col md:w-[450px] md:h-[450px] sm:w-[250px] sm:h-[250px] relative">
+          <Image 
+            src={randomAsheSkin.image} 
+            alt={randomAsheSkin.name} 
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-full h-auto"
+          />
         </div>
-        <div className="flex flex-col p-12 m-12 bg-slate-600 bg-opacity-60">
+        <div className="flex flex-col p-12 m-2 bg-slate-600 bg-opacity-6">
           <div className='grid grid-cols-2'>
             <h3 className='text-2xl'>Name</h3>
             <h3>{randomAsheSkin.name}</h3>
